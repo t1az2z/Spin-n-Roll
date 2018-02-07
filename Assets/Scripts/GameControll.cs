db3f3da23c15ad4267a9238aae0d580d97958bd4
t1az2z <tyukovia@gmail.com>
2018-02-07 10:43:17 +0300

    controlls and collisions work

---
+++ Assets/Scripts/GameControll.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+//Controlls level rotation
+public class GameControll : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 100f;
+
+    public GameObject player;
+
+    private GameManagment gameManagmentAccess;
+
+    
+
+    void Start ()
+    {
+        gameManagmentAccess = player.GetComponent<GameManagment>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (gameManagmentAccess.currentState == GameManagment.State.Alive)
+        {
+            Controls();
+            if (transform.position.y <= -10f) { gameManagmentAccess.StartDeathSequence(); } //preventing from falling out of labirinth
+        }
+    }
+    
+    private void Controls()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameManagment.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+//controlls everything in the game
+public class GameManagment : MonoBehaviour
+{
+
+    public GameObject level;
+    private GameControll gameControllAccess;
+    public enum State { Alive, Dead, Transcending };
+    public State currentState = State.Alive;
+    [SerializeField] float levelLoadDelay = 3f;
+
+    
+    // Use this for initialization
+    void Start ()
+    {
+        gameControllAccess = level.GetComponent<GameControll>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+            {
+                case "Finish":
+                    StartSuccessSequence();
+                    break;
+                case "Enemy":
+                    StartDeathSequence();
+                    break;
+                case "Fire":
+                     //todo create fire effect
+                    break;
+            }
+        }
+ 
+
+    public void StartSuccessSequence()
+    {
+        currentState = State.Transcending;
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
+
+    public void StartDeathSequence()
+    {
+        currentState = State.Dead;
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
+
+    void LoadNextScene()
+    {
+        if (currentState == State.Transcending)
+        {
+            print("Level compllete!");
+            LoadLevel(0);
+        }
+        else if (currentState == State.Dead)
+        {
+            print("You died!");
+            LoadLevel(0);
+        }
+    }
+    private void LoadLevel(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+}
